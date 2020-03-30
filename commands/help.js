c7fcfd5cc8ad9dbef8659c3400c2dde0f3258e7f
Cody Bender <50554676+cfbender@users.noreply.github.com>
2020-03-29 21:47:09 -0600

    Add today to help

--- commands/help.js
+++ commands/help.js
@@ -8,14 +8,18 @@ const execute = (msg, args) => {
       "https://vignette3.wikia.nocookie.net/animalcrossing/images/b/b7/Turnip.png/revision/latest?cb=20100726212427"
     )
     .setTimestamp()
-    .addField("**Stats**", "Shows all-time stats. Usage: stalk! stats")
+    .addField("**Stats**", "Shows all-time stats. Usage: `stalk! stats\`")
     .addField(
       "**Selling**",
-      "Registers a price from Daisy Mae. Usage: stalk! selling <price>"
+      "Registers a price from Daisy Mae. Usage: `stalk! selling <price>`"
     )
     .addField(
       "**Buying**",
-      "Registers a price from Timmy Nook. Usage: stalk! buying <price>"
+      "Registers a price from Timmy Nook. Usage: `stalk! buying <price>`"
+    )
+    .addField(
+      "**Today**",
+      "Shows all stats and prices for the last day prices were added. Usage: `stalk! today`"
     );
 
   msg.channel.send(embed);
